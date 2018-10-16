2644cb4cfe458b3412b3c14c124e8c7982b63770
Henrik Ingbrant Björs <b0h@cock.li>
2018-10-16 21:06:18 +0200

    Tweaking keybindings a bit

--- keybinds.el
+++ keybinds.el
@@ -1,7 +1,3 @@
 (global-set-key (kbd "C-c r") 'ruby-eval-region-message)
-(global-set-key (kbd "C-c C-r") 'ruby-eval-region-replace)
-(global-set-key (kbd "C-c M-r") 'ruby-eval-region-insert)
-
-;; Superior navigation
-(global-set-key (kbd "M-n") 'forward-paragraph)
-(global-set-key (kbd "M-p") 'backward-paragraph)
+(global-set-key (kbd "C-c R") 'ruby-eval-region-replace)
+(global-set-key (kbd "C-c I") 'ruby-eval-region-insert)
