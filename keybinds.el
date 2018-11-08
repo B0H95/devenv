6d2e0b76768569a20b22898842dc45fd6744d34c
Henrik Ingbrant Björs <b0h@cock.li>
2018-11-08 22:08:46 +0100

    Bind the useful dabbrev commands to the function keys

--- keybinds.el
+++ keybinds.el
@@ -1,3 +1,6 @@
 (global-set-key (kbd "C-c r") 'ruby-eval-region-message)
 (global-set-key (kbd "C-c R") 'ruby-eval-region-replace)
 (global-set-key (kbd "C-c I") 'ruby-eval-region-insert)
+
+(global-set-key (kbd "<f5>") 'dabbrev-completion)
+(global-set-key (kbd "<f6>") 'dabbrev-expand)
