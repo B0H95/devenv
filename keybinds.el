277a62eeb151f0b97f6c2aefde5283e959820563
Henrik Ingbrant Björs <b0h@cock.li>
2018-11-16 12:31:24 +0100

    Map dabbrev commands to TAB instead of function keys

--- keybinds.el
+++ keybinds.el
@@ -2,5 +2,5 @@
 (global-set-key (kbd "C-c R") 'ruby-eval-region-replace)
 (global-set-key (kbd "C-c I") 'ruby-eval-region-insert)
 
-(global-set-key (kbd "<f5>") 'dabbrev-completion)
-(global-set-key (kbd "<f6>") 'dabbrev-expand)
+(global-set-key (kbd "<C-tab>") 'dabbrev-completion)
+(global-set-key (kbd "<backtab>") 'dabbrev-expand)
